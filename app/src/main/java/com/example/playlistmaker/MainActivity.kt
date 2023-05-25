a9ae644f330701a3b6ebf8a3ebf3b0a70d7d16bc
AShipilov <thesol590@yandex.ru>
2023-05-26 00:48:11 +0300

    Домашнее задание, исправления.

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -3,42 +3,47 @@ package com.example.playlistmaker
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.button.MaterialButton
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val searchButton = findViewById<Button>(R.id.search_button)
-        searchButton.setOnClickListener(this@MainActivity)
-        val mediatekaButton = findViewById<Button>(R.id.mediateka_button)
-        mediatekaButton.setOnClickListener(this@MainActivity)
-        val settingsButton = findViewById<Button>(R.id.settings_button)
-        settingsButton.setOnClickListener(this@MainActivity)
+        val searchButton = findViewById<MaterialButton>(R.id.search_button)
+        searchButton.setOnClickListener {
+            val displayButton = Intent(this, SearchActivity::class.java)
+            startActivity(displayButton)
+        }
+        val mediatekaButton = findViewById<MaterialButton>(R.id.mediateka_button)
+        mediatekaButton.setOnClickListener {
+            val displayButton = Intent(this, MediatekaActivity::class.java)
+            startActivity(displayButton)
+        }
+        val settingsButton = findViewById<MaterialButton>(R.id.settings_button)
+        settingsButton.setOnClickListener {
+            val displayButton = Intent(this, SettingsActivity::class.java)
+            startActivity(displayButton)
+        }
+
     }
 
     override fun onClick(v: View?) {
         when (v?.id) {
             R.id.search_button -> {
-                v?.setOnClickListener {
-                    val displayButton = Intent(this, SearchActivity::class.java)
-                    startActivity(displayButton)
-                }
+                val displayButton = Intent(this, SearchActivity::class.java)
+                startActivity(displayButton)
             }
 
             R.id.mediateka_button -> {
-                v?.setOnClickListener {
-                    val displayButton = Intent(this, MediatekaActivity::class.java)
-                    startActivity(displayButton)
-                }
+                val displayButton = Intent(this, MediatekaActivity::class.java)
+                startActivity(displayButton)
+
             }
 
             R.id.settings_button -> {
-                v?.setOnClickListener {
-                    val displayIntent = Intent(this, SettingsActivity::class.java)
-                    startActivity(displayIntent)
-                }
+                val displayButton = Intent(this, SettingsActivity::class.java)
+                startActivity(displayButton)
             }
         }
     }
