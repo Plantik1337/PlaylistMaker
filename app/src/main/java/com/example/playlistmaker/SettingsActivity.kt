2aae46ec0234099df7358c158553f9420f0b722a
AShipilov <thesol590@yandex.ru>
2023-06-01 17:26:41 +0300

    Домашнее задание. нажатия на кнопки на экране настроек

--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -2,26 +2,50 @@ package com.example.playlistmaker
 
 import android.app.Activity
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import android.view.View
-import android.widget.ImageButton
 import com.google.android.material.button.MaterialButton
 
-class SettingsActivity : Activity(), View.OnClickListener {
+
+class SettingsActivity : Activity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.settings_screen)
-        val back = findViewById<MaterialButton>(R.id.backToMainActivity)
-        back.setOnClickListener {
+
+        findViewById<MaterialButton>(R.id.backToMainActivity).setOnClickListener {// Выход с экрана настроек
             finish()
         }
-    }
 
-    override fun onClick(v: View?) {
-        when (v?.id) {
-            R.id.backToMainActivity -> {
-                finish()
+        findViewById<MaterialButton>(R.id.shareButton).setOnClickListener {//Поделиться
+            val courseLink = "https://praktikum.yandex.ru/android-developer/"
+            val sendIntent = Intent().apply {
+                action = Intent.ACTION_SEND
+                putExtra(Intent.EXTRA_TEXT, courseLink)
+                type = "text/plain"
             }
+            startActivity(Intent.createChooser(sendIntent, null))
+        }
+        findViewById<MaterialButton>(R.id.contactUs).setOnClickListener {// Связь с техподдержкой
+            val intent = Intent(Intent.ACTION_SENDTO).apply {
+                data = Uri.parse("mailto:") // тип данных для отправки
+                putExtra(Intent.EXTRA_EMAIL, arrayOf("Thesol590@yandex.ru"))// почта
+                putExtra(
+                    Intent.EXTRA_SUBJECT,
+                    "Сообщение разработчикам и разработчицам приложения Playlist Maker" // Тема сообщения
+                )
+                putExtra(
+                    Intent.EXTRA_TEXT,
+                    "Спасибо разработчикам и разработчицам за крутое приложение!" // Текст сообщения
+                )
+            }
+            startActivity(intent)
+        }
+        findViewById<MaterialButton>(R.id.termsOfUse).setOnClickListener {// Пользовательское соглашение
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data =
+                Uri.parse("https://yandex.ru/legal/practicum_offer/") // Ссылка на пользовательское соглашение
+            startActivity(intent)
         }
     }
 }
