555c03c7b6a52df0b7799c6a86080b4563b6b83a
Ashipilov <Thesol590@yandex.ru>
2023-07-05 21:31:53 +0300

    Домашнее задание заглушка, recyclerView

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -5,10 +5,13 @@ import android.os.Bundle
 import android.os.PersistableBundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -27,11 +30,44 @@ class SearchActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
 
+        lateinit var adapter: Adapter
+
         val editText = findViewById<EditText>(R.id.searchEditText)
         val imageViewButton = findViewById<ImageView>(R.id.cancelInputSearchEditText)
         val inputMethod = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
-        val trackList = findViewById<RecyclerView>(R.id.trackListRecyclerView)
-
+        val trackListView = findViewById<RecyclerView>(R.id.trackListRecyclerView)
+        val trackList: ArrayList<Track> = arrayListOf(
+            Track(
+                trackName = "Smells Like Teen Spirit",
+                artistName = "Nirvana",
+                trackTime = "5:01",
+                artworkUrl100 = "https://is5-ssl.mzstatic.com/image/thumb/Music115/v4/7b/58/c2/7b58c21a-2b51-2bb2-e59a-9bb9b96ad8c3/00602567924166.rgb.jpg/100x100bb.jpg"
+            ),
+            Track(
+                trackName = "Billie Jean",
+                artistName = "Michael Jackson",
+                trackTime = "4:35",
+                artworkUrl100 = "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/3d/9d/38/3d9d3811-71f0-3a0e-1ada-3004e56ff852/827969428726.jpg/100x100bb.jpg"
+            ),
+            Track(
+                trackName = "Stayin' Alive",
+                artistName = "Bee Gees",
+                trackTime = "4:10",
+                artworkUrl100 = "https://is4-ssl.mzstatic.com/image/thumb/Music115/v4/1f/80/1f/1f801fc1-8c0f-ea3e-d3e5-387c6619619e/16UMGIM86640.rgb.jpg/100x100bb.jpg"
+            ),
+            Track(
+                trackName = "Whole Lotta Love",
+                artistName = "Led Zeppelin",
+                trackTime = "5:33",
+                artworkUrl100 = "https://is2-ssl.mzstatic.com/image/thumb/Music62/v4/7e/17/e3/7e17e33f-2efa-2a36-e916-7f808576cf6b/mzm.fyigqcbs.jpg/100x100bb.jpg"
+            ),
+            Track(
+                trackName = "Sweet Child O'Mine",
+                artistName = "Guns N' Roses",
+                trackTime = "5:03",
+                artworkUrl100 = "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/a0/4d/c4/a04dc484-03cc-02aa-fa82-5334fcb4bc16/18UMGIM24878.rgb.jpg/100x100bb.jpg"
+            )
+        )
         findViewById<MaterialButton>(R.id.backToMainActivityFromSearchActivity).setOnClickListener() {
             finish()
         }
@@ -61,8 +97,12 @@ class SearchActivity : AppCompatActivity() {
             savedTextSearch = savedInstanceState.getString(EDIT_TEXT, "")
             editText.setText(savedTextSearch)
         }
-        trackList.layoutManager = LinearLayoutManager(this)
-    }
+        trackListView.layoutManager = LinearLayoutManager(this)
+
+        adapter = Adapter(trackList)
+        trackListView.adapter = adapter
+
+    }//
 
     override fun onSaveInstanceState(outState: Bundle, outPersistentState: PersistableBundle) {
         super.onSaveInstanceState(outState, outPersistentState)
@@ -78,38 +118,7 @@ class SearchActivity : AppCompatActivity() {
         editText.setText(savedTextSearch)
     }
 
-    val trackList: ArrayList<Track> = arrayListOf(
-        Track(
-            trackName = "Smells Like Teen Spirit",
-            artistName = "Nirvana",
-            trackTime = "5:01",
-            artworkUrl100 = "https://is5-ssl.mzstatic.com/image/thumb/Music115/v4/7b/58/c2/7b58c21a-2b51-2bb2-e59a-9bb9b96ad8c3/00602567924166.rgb.jpg/100x100bb.jpg"
-        ),
-        Track(
-            trackName = "Billie Jean",
-            artistName = "Michael Jackson",
-            trackTime = "4:35",
-            artworkUrl100 = "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/3d/9d/38/3d9d3811-71f0-3a0e-1ada-3004e56ff852/827969428726.jpg/100x100bb.jpg"
-        ),
-        Track(
-            trackName = "Stayin' Alive",
-            artistName = "Bee Gees",
-            trackTime = "4:10",
-            artworkUrl100 = "https://is4-ssl.mzstatic.com/image/thumb/Music115/v4/1f/80/1f/1f801fc1-8c0f-ea3e-d3e5-387c6619619e/16UMGIM86640.rgb.jpg/100x100bb.jpg"
-        ),
-        Track(
-            trackName = "Whole Lotta Love",
-            artistName = "Led Zeppelin",
-            trackTime = "5:33",
-            artworkUrl100 = "https://is2-ssl.mzstatic.com/image/thumb/Music62/v4/7e/17/e3/7e17e33f-2efa-2a36-e916-7f808576cf6b/mzm.fyigqcbs.jpg/100x100bb.jpg"
-        ),
-        Track(
-            trackName = "Sweet Child O'Mine",
-            artistName = "Guns N' Roses",
-            trackTime = "5:03",
-            artworkUrl100 = "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/a0/4d/c4/a04dc484-03cc-02aa-fa82-5334fcb4bc16/18UMGIM24878.rgb.jpg/100x100bb.jpg"
-        )
-    )
+
 
 }
 
@@ -119,3 +128,36 @@ data class Track(
     val trackTime: String, // Продолжительность трека
     val artworkUrl100: String // Ссылка на изображение обложки
 )
+
+class Adapter(
+    private val track: ArrayList<Track>
+): RecyclerView.Adapter<Adapter.TrackViewHolder>(){
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Adapter.TrackViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.v_track_line, parent, false)
+        return TrackViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: TrackViewHolder, position: Int) {
+        holder.bind(track[position])
+    }
+
+    override fun getItemCount(): Int {
+        return track.size
+    }
+    class TrackViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+        private val albumImage: ImageView = itemView.findViewById(R.id.albumImageView)
+        private val trackName: TextView = itemView.findViewById(R.id.trackNameTextView)
+        private val artistName: TextView = itemView.findViewById(R.id.artistNameTextView)
+        private val songDuration: TextView = itemView.findViewById(R.id.songDurationTextView)
+
+        fun bind(track: Track){
+            //albumImage.setImageResource(track.artworkUrl100)
+            trackName.text = track.trackName
+            artistName.text = track.artistName
+            songDuration.text = track.trackTime
+
+        }
+    }
+}
+
+
