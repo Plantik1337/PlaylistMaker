471b2fb1e8562cf27e96388075b4a27b4d6548f7
Ashipilov <thesol5900@gmail.com>
2023-07-24 14:51:28 +0300

    Заглушки у обложек

--- app/src/main/java/com/example/playlistmaker/Adapter.kt
+++ app/src/main/java/com/example/playlistmaker/Adapter.kt
@@ -1,6 +1,8 @@
 package com.example.playlistmaker
 
 import android.graphics.Outline
+import android.graphics.drawable.Drawable
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,6 +11,8 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import java.text.SimpleDateFormat
+import java.util.Locale
 
 class Adapter(
     private val track: ArrayList<Track>
@@ -31,13 +35,17 @@ class Adapter(
         private val songDuration: TextView = itemView.findViewById(R.id.songDurationTextView)
 
         fun bind(track: Track) {
-            val maxSymbols = 32
+            val maxSymbols = 30
             if (track.trackName.length > maxSymbols) {
                 trackName.text = track.trackName.substring(0, maxSymbols) + "..."
             } else {
                 trackName.text = track.trackName
             }
-            Glide.with(itemView).load(track.artworkUrl100).into(albumImage)
+            Glide.with(itemView)
+                .load(track.artworkUrl100)
+                .placeholder(R.drawable.placeholder)
+                .error(R.drawable.placeholder)
+                .into(albumImage)
             albumImage.clipToOutline = true
             albumImage.outlineProvider = object : ViewOutlineProvider() {
                 override fun getOutline(view: View, outline: Outline) {
@@ -48,8 +56,16 @@ class Adapter(
 
             artistName.text = track.artistName
 
-            songDuration.text = track.trackTimeMillis
-            //songDuration.text = SimpleDateFormat("mm:ss", Locale.getDefault()).format(track.trackTimeMillis)
+            if (track.artistName.length > maxSymbols) {
+                artistName.text = track.artistName.substring(0, maxSymbols) + "..."
+            } else {
+                artistName.text = track.artistName
+            }
+
+            songDuration.text = SimpleDateFormat(
+                "mm:ss",
+                Locale.getDefault()
+            ).format(track.trackTimeMillis.toLong())
 
         }
     }
--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -51,40 +51,6 @@ class SearchActivity : AppCompatActivity() {
             .addConverterFactory(GsonConverterFactory.create())
             .build()
 
-
-
-//        val trackList: ArrayList<Track> = arrayListOf(
-//            Track(
-//                trackName = "Smells Like Teen Spirit (REMASTERED 2011)",//дописал для проверки вывода
-//                artistName = "Nirvana",
-//                trackTime = "5:01",
-//                artworkUrl100 = "https://is5-ssl.mzstatic.com/image/thumb/Music115/v4/7b/58/c2/7b58c21a-2b51-2bb2-e59a-9bb9b96ad8c3/00602567924166.rgb.jpg/100x100bb.jpg"
-//            ),
-//            Track(
-//                trackName = "Billie Jean",
-//                artistName = "Michael Jackson",
-//                trackTime = "4:35",
-//                artworkUrl100 = "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/3d/9d/38/3d9d3811-71f0-3a0e-1ada-3004e56ff852/827969428726.jpg/100x100bb.jpg"
-//            ),
-//            Track(
-//                trackName = "Stayin' Alive",
-//                artistName = "Bee Gees",
-//                trackTime = "4:10",
-//                artworkUrl100 = "https://is4-ssl.mzstatic.com/image/thumb/Music115/v4/1f/80/1f/1f801fc1-8c0f-ea3e-d3e5-387c6619619e/16UMGIM86640.rgb.jpg/100x100bb.jpg"
-//            ),
-//            Track(
-//                trackName = "Whole Lotta Love",
-//                artistName = "Led Zeppelin",
-//                trackTime = "5:33",
-//                artworkUrl100 = "https://is2-ssl.mzstatic.com/image/thumb/Music62/v4/7e/17/e3/7e17e33f-2efa-2a36-e916-7f808576cf6b/mzm.fyigqcbs.jpg/100x100bb.jpg"
-//            ),
-//            Track(
-//                trackName = "Sweet Child O'Mine",
-//                artistName = "Guns N' Roses",
-//                trackTime = "5:03",
-//                artworkUrl100 = "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/a0/4d/c4/a04dc484-03cc-02aa-fa82-5334fcb4bc16/18UMGIM24878.rgb.jpg/100x100bb.jpg"
-//            )
-//        )
         findViewById<MaterialButton>(R.id.backToMainActivityFromSearchActivity).setOnClickListener {
             finish()
         }
@@ -111,27 +77,41 @@ class SearchActivity : AppCompatActivity() {
         editText.setOnEditorActionListener { _, actionId, _ ->
             if (actionId == EditorInfo.IME_ACTION_DONE) {
                 // ВЫПОЛНЯЙТЕ ПОИСКОВЫЙ ЗАПРОС ЗДЕСЬ
+
                 val appleMusicServer = retrofit.create(AppleMusicServer::class.java)
 
-                appleMusicServer.search(editText.text.toString()).enqueue(object : Callback<MusicResponse>{
-                    override fun onResponse(call: Call<MusicResponse>, response: Response<MusicResponse>) {
-                        Log.d(TAG, "onResponse: ${response.body()?.results}")
-                        if(response.body()!=null) {
-                            val musicCollection = response.body()!!.results
-                            adapter = Adapter(musicCollection)
-                            trackListView.adapter = adapter
-                            adapter.notifyDataSetChanged()
-                        }
-                        else{
-                            Log.e(TAG,"Что-то пошло не так, серер не отдаёт список песен")
+                appleMusicServer.search(editText.text.toString())
+                    .enqueue(object : Callback<MusicResponse> {
+                        override fun onResponse(
+                            call: Call<MusicResponse>,
+                            response: Response<MusicResponse>
+                        ) {
+                            Log.d(TAG, "onResponse: ${response.body()?.results}")
+                            when (response.code()) {
+                                200 -> { // песни найдены
+                                    val musicCollection = response.body()!!.results
+                                    adapter = Adapter(musicCollection)
+                                    trackListView.adapter = adapter
+                                    //adapter.notifyDataSetChanged()
+                                }
+
+                                204 -> { // песни не найдены
+                                    TODO("Вывести заглушку, которая сообщает о том, что песни по запросу не найдены")
+                                }
+                            }
+//                            if (response.body() != null) {
+//
+//                            } else {
+//                                Log.e(TAG, "Что-то пошло не так, серер не отдаёт список песен")
+//                            }
+
                         }
 
-                    }
-
-                    override fun onFailure(call: Call<MusicResponse>, t: Throwable) {
-                        Log.d(TAG, "onFailure: $t")
-                    }
-                })
+                        override fun onFailure(call: Call<MusicResponse>, t: Throwable) {
+                            Log.d(TAG, "onFailure: $t")
+                           // TODO("Вывести сообщение об ошибке подключения")
+                        }
+                    })
 
                 true
             }
@@ -139,6 +119,8 @@ class SearchActivity : AppCompatActivity() {
         }
         imageViewButton.setOnClickListener {
             editText.setText("")
+            //trackListView.adapter.clear()
+            //trackListView.recycledViewPool.clear()
             inputMethod.hideSoftInputFromWindow(editText.windowToken, 0)
         }
         if (savedInstanceState != null) {
@@ -168,6 +150,6 @@ class SearchActivity : AppCompatActivity() {
 
 interface AppleMusicServer {
     @GET("/search?entity=song")
-    fun search(@Query("term")text: String):Call<MusicResponse>
+    fun search(@Query("term") text: String): Call<MusicResponse>
 }
 
